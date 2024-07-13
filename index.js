877b183635ae0c962dcd758e2810144b3b310e7c
Javidanhaj <hacizadecavidan.ch@gmail.com>
2024-07-13 15:39:05 +0400

    added

---
+++ index.js
@@ -0,0 +1,21 @@
+
+const mybutton = document.getElementById('mybutton');
+const label1 = document.getElementById('label1');
+const label2 = document.getElementById('label2');
+const label3 = document.getElementById('label3');
+const min = 1;
+const max = window.prompt("Enter the maximum number: ");
+
+let randomNum1;
+let randomNum2;
+let randomNum3;
+
+
+mybutton.onclick = function() {
+    randomNum1 = Math.floor(Math.random() * max) + min;
+    randomNum2 = Math.floor(Math.random() * max) + min;
+    randomNum3 = Math.floor(Math.random() * max) + min;
+    label1.textContent = randomNum1;
+    label2.textContent = randomNum2;
+    label3.textContent = randomNum3;
+}
